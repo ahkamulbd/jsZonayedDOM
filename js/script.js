d5785690a55d595be80e7399ce5712eeb603cdea
ahkamulbd <ahkamulbd@gmail.com>
2023-08-26 18:03:08 +0600

    Upto adding Style i.e. # 5 addressed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
-console.log('Script to address DOM in JS.')
+//console.log('Script to address DOM in JS.')
+
+let boxOne = document.querySelector('#box-1');
+//console.dir(boxOne);
+
+let boxTwo = document.getElementById('box-2');
+
+let boxNodeList = document.querySelectorAll('.box');
+//console.dir(boxNodeList);
+let boxArray = Array.prototype.slice.call(boxNodeList);
+//console.dir(boxArray);
+
+let boxHtmlSelection = document.getElementsByClassName('div');
+console.log(boxHtmlSelection);
+
+let sectionContainer = document.getElementById('section-container');
+
+/* ====== Create Element and Append Child ====== */
+
+let newDiv = document.createElement('div');
+newDiv.innerText = 'New Div';
+newDiv.classList.add('box');
+sectionContainer.appendChild(newDiv);
+console.log(newDiv);
+
+/* ====== Adding Style ====== */
+
+boxTwo.style.backgroundColor = 'lightGreen';
+boxTwo.style.color = 'orange';
+boxTwo.style.fontSize = '40px';
+
+/* ====== Adding id in newly created Element ====== */
+
+let addedDiv = document.createElement('div');
+addedDiv.classList.add('box');
+addedDiv.id = 'added-id';
+addedDiv.innerHTML = `
+    <div>Added Div</div>
+`;
+addedDiv.style.color = 'yellow';
+sectionContainer.appendChild(addedDiv);
